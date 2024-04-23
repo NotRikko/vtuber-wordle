5be44c3dac89e9d5226add5315d10a1f76c4d639
Rikko <bulletbill51@yahoo.com>
2024-04-22 23:35:18 -0700

    Fixed issues with navbar styling
    Changed flexbox to css grid

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import Style from './Navbar.module.css'
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
 
 function Navbar() {
     return (
         <nav className={Style.nav}>
-            <ul className={Style.header}>
-                <li><MenuRoundedIcon /></li>
-                <li style={ {fontSize: "1.4rem" }}>Vordle</li>
-                <div className={Style.rightHeader}>
-                    <li><LightModeRoundedIcon /></li>
-                    <li><SettingsRoundedIcon /></li>
-                </div>
+            <li className={Style.leftHeader}><MenuRoundedIcon /></li>
+            <li className={Style.logo}>Vordle</li>
+            <ul className={Style.rightHeader}>
+                <li><BarChartRoundedIcon /></li>
+                <li><LightModeRoundedIcon /></li>
+                <li><SettingsRoundedIcon /></li>
             </ul>
         </nav>
     )
