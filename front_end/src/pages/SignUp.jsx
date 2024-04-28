a8b97413d06a7e881f13af684da1b3300ddadf37
Rikko <bulletbill51@yahoo.com>
2024-04-28 04:03:46 -0700

    gitignore changed

--- front_end/src/pages/SignUp.jsx
+++ front_end/src/pages/SignUp.jsx
@@ -4,15 +4,19 @@ function SignUp() {
     return (
         <div id={Style.signup_page}>
             <div id={Style.signup}>
-                <div>
+                <div id={Style.signup_header}>
                     <h1>Join Vordle</h1>
                     <h2>Keep track of your scores</h2>
-                    <h3>Already have an account? <a href='' style={{ color: 'blue' }}>Log in.</a></h3>
+                    <h2>Already have an account? <a href='' style={{ color: 'rgb(155, 185, 177)' }}>Log in.</a></h2>
                 </div>
                 <form id={Style.signup_form} action='/signup'>
                     <div className={Style.signup_sec}>
                         <label for='user_name'>Username</label>
                         <input type='text' name='user_name'/>
+                    </div>
+                    <div className={Style.signup_sec}>
+                        <label for='email'>Email</label>
+                        <input type='email' name='email'/>
                     </div>
                     <div className={Style.signup_sec}>
                         <label for='password'>Password</label>
@@ -22,12 +26,9 @@ function SignUp() {
                         <label for='confirm_password'>Confirm Password</label>
                         <input type='password' name='confirm_password'/>
                     </div>
-                    <div className={Style.signup_sec}>
-                        <label for='email'>Email</label>
-                        <input type='email' name='email'/>
-                    </div>
                     <button type='submit'>Create Account</button>
                 </form>
+                <p>Thank you for joining and supporting the Vordle community.</p>
             </div>
         </div>
     )
