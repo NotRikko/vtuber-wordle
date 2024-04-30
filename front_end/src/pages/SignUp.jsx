f6b2d12f77b9e78b55ab48d5435bbb6456f3f06b
Rikko <bulletbill51@yahoo.com>
2024-04-29 22:08:30 -0700

    Added useState to handle form data in signup page
    form now uses fetch to post data

--- front_end/src/pages/SignUp.jsx
+++ front_end/src/pages/SignUp.jsx
@@ -1,6 +1,42 @@
+import { useState } from 'react'
 import Style from './Game.module.css'
 
 function SignUp() {
+    
+    const [formData, setFormData] = useState({
+        user_name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit =  async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3000/users/user_create_post', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(formData),
+            });
+            if(!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            const data = await response.json();
+            console.log(data);
+        } catch(error) {
+            console.error('Error:', error);
+        }
+        
+    };
+
     return (
         <div id={Style.signup_page}>
             <div id={Style.signup}>
@@ -9,22 +45,40 @@ function SignUp() {
                     <h2>Keep track of your scores</h2>
                     <h2>Already have an account? <a href='' style={{ color: 'rgb(155, 185, 177)' }}>Log in.</a></h2>
                 </div>
-                <form id={Style.signup_form} action='/signup'>
+                <form id={Style.signup_form} onSubmit={handleSubmit}>
                     <div className={Style.signup_sec}>
                         <label for='user_name'>Username</label>
-                        <input type='text' name='user_name'/>
+                        <input 
+                            type='text' 
+                            name='user_name' 
+                            required
+                            value={formData.user_name}
+                            onChange={handleInputChange}
+                        />
                     </div>
                     <div className={Style.signup_sec}>
                         <label for='email'>Email</label>
-                        <input type='email' name='email'/>
+                        <input 
+                        type='email' 
+                        name='email' 
+                        required
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        />
                     </div>
                     <div className={Style.signup_sec}>
                         <label for='password'>Password</label>
-                        <input type='password' name='password'/>
+                        <input 
+                        type='password' 
+                        name='password' 
+                        required
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        />
                     </div>
                     <div className={Style.signup_sec}>
                         <label for='confirm_password'>Confirm Password</label>
-                        <input type='password' name='confirm_password'/>
+                        <input type='password' name='confirm_password' required/>
                     </div>
                     <button type='submit'>Create Account</button>
                 </form>
